910ed05ae947d162febb01f099a69734862974af
alor <alor@hackingteam.it>
2011-03-04 12:25:31 +0100

    flatsingleton implementation

---
+++ test/test_flatsingleton.rb
@@ -0,0 +1,20 @@
+require "helper"
+
+require 'rcs-common/flatsingleton'
+require 'singleton'
+
+class TestSingleton
+  include Singleton
+  extend FlatSingleton
+
+  def test
+    return 'flat test succeeded'
+  end
+end
+
+class FlatSingletonTest < Test::Unit::TestCase
+
+  def test_flat_singleton
+    assert_equal 'flat test succeeded', TestSingleton.test
+  end
+end
