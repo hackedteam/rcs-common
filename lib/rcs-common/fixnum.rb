f2d662d1743d6a921a874a9bb8f3fbfbaa62ddeb
alor <alor@hackingteam.it>
2011-03-09 09:10:51 +0100

    refactoring

--- lib/rcs-common/fixnum.rb
+++ lib/rcs-common/fixnum.rb
@@ -1,24 +1,34 @@
 
 module ByteSize
 
-  KiB = 1024
-  MiB = KiB * 1024
-  GiB = MiB * 1024
-  TiB = GiB * 1024
+  KiB = 2**10
+  MiB = 2**20
+  GiB = 2**30
+  TiB = 2**40
 
-  def to_s_bytes
-    # return the size in a human readable format
-    if self >= TiB then
-      return (self.to_f / TiB).round(2).to_s + ' TiB'
-    elsif self >= GiB then
-      return (self.to_f / GiB).round(2).to_s + ' GiB'
-    elsif self >= MiB
-      return (self.to_f / MiB).round(2).to_s + ' MiB'
-    elsif self >= KiB
-      return (self.to_f / KiB).round(2).to_s + ' KiB'
-    else
-      return self.to_s + ' B'
+  KB = 10**3
+  MB = 10**6
+  GB = 10**9
+  TB = 10**12
+
+  # return the size in a human readable format
+  def to_s_bytes(base = 2)
+
+    base_two = {TiB => 'TiB', GiB => 'GiB', MiB => 'MiB', KiB => 'KiB'}
+    base_ten = {TB => 'TB', GB => 'GB', MB => 'MB', KB => 'kB'}
+
+    values = base_two if base == 2
+    values = base_ten if base == 10
+
+    values.each_pair do |k, v|
+      if self >= k
+        return (self.to_f / k).round(2).to_s + ' ' + v
+      end
     end
+
+    # case when is under KiB
+    return self.to_s + ' B'
+
   end
 
 end
@@ -28,7 +38,7 @@ class Fixnum
 end
 
 # we need to add it even to Bignum for windows32 compatibility
-# everything ove a GiB is Bignum...
+# everything over a GiB is Bignum...
 class Bignum
   include ByteSize
 end
