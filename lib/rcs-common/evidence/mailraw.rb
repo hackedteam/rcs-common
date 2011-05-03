156470e2a0ce5ef95001b4a1a0e39ac11379206c
daniele <d.milan@hackingteam.it>
2011-05-03 17:17:33 +0200

    Mailraw evidence generation.

--- lib/rcs-common/evidence/mailraw.rb
+++ lib/rcs-common/evidence/mailraw.rb
@@ -1,10 +1,38 @@
+require 'rcs-common/evidence/common'
 require 'mail'
-require_relative 'common'
 
 module RCS
 module MailrawEvidence
+
   MAILRAW_VERSION = 2009070301
+
+  ADDRESSES = ['ciccio.pasticcio@google.com', 'billg@microsoft.com', 'john.doe@nasa.gov', 'mario.rossi@italy.it']
+  SUBJECTS = ['drugs', 'bust me!', 'police here']
+  BODIES = ["You're busted, dude.", "I'm a drug trafficker, send me to hang!", "I'll sell meth to kids. Stop me."]
   
+  def content
+    binary = StringIO.new
+    
+    email = Mail.new do
+      from    ADDRESSES.sample
+      to      ADDRESSES.sample
+      subject SUBJECTS.sample
+      body    BODIES.sample
+    end
+    
+    ft_high, ft_low = Time.now.to_filetime
+    body = email.to_s
+    add_header = [MAILRAW_VERSION, 1, body.bytesize, ft_high, ft_low].pack("I*")
+    binary.write(add_header)
+    binary.write(body)
+
+    binary.string
+  end
+
+  def generate_content
+    [ content ]
+  end
+
   def decode_additional_header(data)
     raise EvidenceDeserializeError.new("incomplete MAILRAW") if data.nil? or data.bytesize == 0
     
