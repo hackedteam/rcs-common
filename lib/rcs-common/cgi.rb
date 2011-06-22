6f9aa0650d30d79062ecea661ee47f7d069adf5f
daniele <d.milan@hackingteam.it>
2011-06-22 17:50:56 +0200

    Fixed CGI query encoding.

--- lib/rcs-common/cgi.rb
+++ lib/rcs-common/cgi.rb
@@ -2,6 +2,6 @@ require 'cgi'
 
 class CGI
   def self.encode_query(hash)
-    return "?" + hash.map{|k,v| "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}"}.join("&")
+    return hash.map{|k,v| "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}"}.join("&")
   end
 end
