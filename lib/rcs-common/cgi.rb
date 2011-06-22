297b56c31df4b32e33a52ccad655c486bd1384c7
daniele <d.milan@hackingteam.it>
2011-06-22 16:31:43 +0200

    URI is now CGI.

--- lib/rcs-common/cgi.rb
+++ lib/rcs-common/cgi.rb
@@ -1,11 +1,7 @@
 require 'cgi'
 
-module RCS
-
-class URI
+class CGI
   def self.encode_query(hash)
     return "?" + hash.map{|k,v| "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}"}.join("&")
   end
 end
-
-end # ::RCS
--- test/test_cgi.rb
+++ test/test_cgi.rb
@@ -4,11 +4,11 @@ class URITest < Test::Unit::TestCase
 
   def test_hash_to_uri_query
     h = {'q' => 'pippo', 'filter' => 'pluto', :symbol => 'paperino'}
-    assert_equal "?q=pippo&filter=pluto&symbol=paperino", RCS::URI::encode_query(h)
+    assert_equal "?q=pippo&filter=pluto&symbol=paperino", CGI.encode_query(h)
   end
 
   def test_hash_with_special_chars_to_uri_query
     h = {'q' => 'pippo pluto paperino'}
-    assert_equal "?q=pippo+pluto+paperino", RCS::URI::encode_query(h)
+    assert_equal "?q=pippo+pluto+paperino", CGI.encode_query(h)
   end
 end
