cdcce178698d6f9965da772123770a1765efac0d
daniele <d.milan@hackingteam.it>
2012-05-03 10:09:43 +0200

    fixed test

--- test/test_evidence.rb
+++ test/test_evidence.rb
@@ -22,9 +22,9 @@ class TestEvidence < Test::Unit::TestCase
   # TODO: this test is not really a good one ... tests both generation and deserialization of evidence :(
   def test_generate
     piece = RCS::Evidence.new(@key).generate(:DEVICE, @info)
-    evidence = RCS::Evidence.new(@key).deserialize(piece.binary)
+    evidences, action = RCS::Evidence.new(@key).deserialize(piece.binary)
     
-    assert_equal piece.content.force_encoding('UTF-16LE').encode('UTF-8'), evidence[0].content.force_encoding('UTF-16LE').encode('UTF-8')
+    assert_equal piece.content.force_encoding('UTF-16LE').encode('UTF-8'), evidences[0][:data][:content]
   end
   
   def test_align_to_block_len
