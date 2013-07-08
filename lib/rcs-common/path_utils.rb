19c942cc2aea3736feda20d7de76d7f33058aace
topac <topac@hackingteam.com>
2013-07-08 12:17:04 +0200

    avoid two regexp

--- lib/rcs-common/path_utils.rb
+++ lib/rcs-common/path_utils.rb
@@ -5,13 +5,11 @@ module RCS
       # Requires an encrypted ruby script (rcs-XXX-releases folders) when
       # available, otherwise requires the clean version of it (rcs-XXX folders)
   		def require_release path
-        if path !~ /rcs\-.*/
+        unless path.start_with?("rcs-")
           raise 'path must start with "rcs-"'
         end
 
-        project_folder = path[0..path.index('/')-1]
-
-        if project_folder =~ /rcs\-.+\-release/ and File.exists?(path)
+        if path.include?("-release") and File.exists?(path)
           require path
         else
           require path.gsub('-release', '')
