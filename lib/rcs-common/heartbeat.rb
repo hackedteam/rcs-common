20b7255a05d1c5a4dfc223175f67260e8dc083d3
topac <topac@hackingteam.com>
2013-12-12 12:26:02 +0100

    after_hearthbeat callback + support for collector

--- lib/rcs-common/heartbeat.rb
+++ lib/rcs-common/heartbeat.rb
@@ -7,13 +7,15 @@ module RCS::HeartBeat
     extend RCS::Tracer
     include RCS::Tracer
 
-    def initialize(component_name, component_fullname: nil, before_heartbeat: nil)
+    def initialize(component_name, component_fullname: nil, before_heartbeat: nil, after_heartbeat: nil)
       @component_name     = component_name.to_s.downcase
       @component_fullname = component_fullname || "RCS::#{component_name.to_s.capitalize}"
       @before_heartbeat   = before_heartbeat
+      @after_heartbeat    = after_heartbeat
     end
 
     def ip_addr
+      return '' unless defined?(Socket)
       @ip_addr ||= Socket.gethostname rescue 'unknown'
     end
 
@@ -22,16 +24,36 @@ module RCS::HeartBeat
       raise("You should implement the message method")
     end
 
+    def status
+      RCS::SystemStatus.my_status
+    end
+
     def update_component_status
-      instance_exec(&@before_heartbeat) if @before_heartbeat.respond_to?(:call)
+      if @before_heartbeat.respond_to?(:call)
+        callback_result = instance_exec(&@before_heartbeat)
+        return if callback_result == false
+      end
 
       RCS::SystemStatus.reset
 
-      status = RCS::SystemStatus.my_status
+      status_str = status.to_s.upcase
       stats = {:disk => RCS::SystemStatus.disk_free, :cpu => RCS::SystemStatus.cpu_load, :pcpu => RCS::SystemStatus.my_cpu_load(@component_fullname)}
       msg = RCS::SystemStatus.my_error_msg || message
 
-      ::Status.status_update(@component_fullname, ip_addr, status, msg, stats, @component_name, $version)
+      puts '*'*30
+      puts status_str
+      puts message
+
+      if defined?(::Status)
+        # Db
+        ::Status.status_update(@component_fullname, ip_addr, status_str, msg, stats, @component_name, $version)
+      else
+        # Collector
+        db_class = Object.const_get(@component_fullname)::DB
+        db_class.instance.update_status(@component_fullname, ip_addr, status_str, msg, stats, @component_name, $version)
+      end
+    ensure
+       instance_exec(&@after_heartbeat) if @after_heartbeat.respond_to?(:call)
     end
 
     # Declare the component
@@ -45,12 +67,16 @@ module RCS::HeartBeat
       @before_heartbeat = block
     end
 
+    def self.after_heartbeat(&block)
+      @after_heartbeat = block
+    end
+
     # This method is called from outside
     def self.perform
       raise("Undefined component version") unless $version
       raise("Undefined component name") unless @component_name
 
-      new(@component_name, component_fullname: @component_fullname, before_heartbeat: @before_heartbeat).update_component_status
+      new(@component_name, component_fullname: @component_fullname, before_heartbeat: @before_heartbeat, after_heartbeat: @after_heartbeat).update_component_status
     rescue Interrupt
       trace :fatal, "Heartbeat was interrupted because of a term signal"
     rescue Exception => ex
