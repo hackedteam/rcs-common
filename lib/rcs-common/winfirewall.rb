33e1ce10089e21b792822db80aaeb93acc46963d
topac <topac@hackingteam.com>
2014-01-13 16:42:40 +0100

    windows firewall

---
+++ lib/rcs-common/winfirewall.rb
@@ -0,0 +1,213 @@
+module RCS
+  module Common
+    module WinFirewall
+
+      # Represent a Windows Firewall rule.
+      class Rule
+        ATTRIBUTES = %i[direction action local_ip remote_ip local_port remote_port rule_name protocol profiles enabled grouping edge_traversal]
+
+        RULE_GROUP = 'RCS Firewall Rules'
+
+        attr_reader :attributes
+
+        def initialize(attributes = {})
+          # Default attribute values
+          @attributes = {
+            grouping: RULE_GROUP,
+            protocol: :tcp
+          }
+
+          # Merge default attributes with the given ones
+          # and remove invalid attributes
+          attributes.symbolize_keys! if attributes.respond_to?(:symbolize_keys!)
+          attributes.reject! { |key| !ATTRIBUTES.include?(key) }
+          @attributes.merge!(attributes)
+
+          # Define getters and setters
+          ATTRIBUTES.each do |name|
+            define_singleton_method(name) { @attributes[name] }
+            define_singleton_method("#{name}=") { |value| @attributes[name] = value }
+          end
+        end
+
+        def save
+          command = "firewall add rule #{stringify_attributes}"
+
+          raise "Unable to save firewall rule" unless Advfirewall.call(command).ok?
+        end
+
+        def del
+          only = %i[dir profile program service localip remoteip localport remoteport protocol name]
+
+          command = "firewall delete rule #{stringify_attributes(only)}"
+
+          raise "Unable to delete firewall rule" unless Advfirewall.call(command).ok?
+        end
+
+        # Aliases
+
+        def name; rule_name; end
+        def name=(v); @attributes[:rule_name] = v; end
+
+        private
+
+        def stringify_attributes(only = [])
+          attrs = {
+            name:       name,
+            dir:        direction,
+            action:     action,
+            enable:     enabled,
+            protocol:   protocol,
+            profile:    profiles,
+            remoteip:   remote_ip,
+            localip:    local_ip,
+            localport:  local_port,
+            remoteport: remote_port
+          }
+
+          string = ""
+
+          attrs.each do |key, value|
+            next if only.any? and !only.include?(key)
+            next if value.to_s.strip.empty?
+            next if value == :any
+            value = value.respond_to?(:join) ? value.map(&:to_s).join(',') : "\"#{value}\""
+            string << "#{key}=#{value} "
+          end
+
+          string
+        end
+      end
+
+
+      # Parse the response of the netsh advfirewall command
+      class AdvfirewallResponse < String
+        def parse(block_separator_regexp: nil)
+          blocks = nil
+
+          self.each_line do |line|
+            if line =~ block_separator_regexp
+              blocks ||= []
+              blocks << Hash.new
+            end
+
+            if blocks
+              key, value = *line.strip.scan(/^(.+)\s{2,100}(.+)$/)[0]
+
+              next unless key
+
+              key.strip!
+              key = key[0..-2] if key.end_with?(':')
+              value.strip!
+
+              blocks.last[key] = value
+            end
+          end
+
+          blocks || []
+        end
+
+        def self.normalize(string, method: :underscore)
+          if string.respond_to?(:map)
+            string.map { |v| normalize(v, method: method) }
+          else
+            if method == :numeric
+              string.to_i
+            else
+              return nil if string.blank?
+              string.underscore.gsub(' ', '_').to_sym
+            end
+          end
+        end
+
+        def ok?
+          self.strip =~ /Ok\.\z/
+        end
+      end
+
+
+      class Advfirewall
+        # Send a command to netsh advfirewall
+        def self.debug=(val); @debug = val; end
+
+        def self.call(command)
+          command = "netsh advfirewall #{command.strip}"
+          puts command #if @debug
+          AdvfirewallResponse.new(`#{command}`)
+        end
+      end
+
+
+      extend self
+
+
+      # Return :on or :off depending of the firewall state
+      #
+      # Note that the files test/fixtures/advfirewall/show_currentprofile_state_on and
+      # test/fixtures/advfirewall/show_currentprofile_state_off contains an example of the command output
+      def status
+        list = Advfirewall.call("show currentprofile state").parse(block_separator_regexp: /^Public Profile Settings:.+$/)
+        list[0]['State'] == 'ON' ? :on : :off
+      end
+
+      # Enable or the disable the firewall. The netsh command requires elevation to succeed
+      # Accpeted values are :on or :off
+      def status=(state)
+        raise "Invalid state" unless %w[on off].include?(state.to_s)
+        response = Advfirewall.call("set currentprofile state #{state}")
+        raise "Unable to change firewall state: #{response}" unless response.ok?
+      end
+
+      # Returns and array of #Rule
+      #
+      # Note that the files test/fixtures/advfirewall/firewall_show_rule_name_all
+      # contains an example of the command output
+      def rules
+        list = Advfirewall.call("firewall show rule name=all").parse(block_separator_regexp: /^Rule Name:.+$/)
+
+        list.map do |hash|
+          hash.keys.each do |key|
+            new_key, new_value = normalize(key), hash.delete(key)
+
+            new_value = new_value.split(",").map(&:strip) if new_value.include?(",")
+
+            hash[new_key] = if new_value == 'Any'
+               normalize(new_value)
+            elsif %i[local_ip remote_ip name grouping rule_name].include?(new_key)
+              new_value
+            elsif %i[local_port remote_port].include?(new_key)
+              normalize(new_value, method: :numeric)
+            else
+              normalize(new_value)
+            end
+          end
+
+          Rule.new(hash)
+        end
+      end
+
+      def add_rule(attributes)
+        Rule.new(attributes).save
+      end
+
+      def del_rule(name)
+        deleted = 0
+
+        rules.each do |rule|
+          if (name.kind_of?(Regexp) and rule.name =~ name) or (rule.name == name)
+            rule.del
+            deleted += 1
+          end
+        end
+
+        deleted
+      end
+
+      private
+
+      def normalize(string, **attributes)
+        AdvfirewallResponse.normalize(string, attributes)
+      end
+    end
+  end
+end
---
+++ test/test_winfirewall.rb
@@ -0,0 +1,101 @@
+# encoding: utf-8
+
+require 'helper'
+require 'securerandom'
+
+require 'rcs-common/winfirewall'
+
+# The #undescore method is added by active_support/core_ext/string/inflections
+# Here's a very simple implementation
+class String
+  def underscore
+    word = self.to_s.dup
+    word.gsub!(/::/, '/')
+    # word.gsub!(/(?:([A-Za-z\d])|^)(#{inflections.acronym_regex})(?=\b|[^a-z])/) { "#{$1}#{$1 && '_'}#{$2.downcase}" }
+    word.gsub!(/([A-Z\d]+)([A-Z][a-z])/,'\1_\2')
+    word.gsub!(/([a-z\d])([A-Z])/,'\1_\2')
+    word.gsub!("-", "_")
+    word.downcase!
+    word
+  end
+end
+
+class WinFirewallTest < Test::Unit::TestCase
+  def subject
+    @subject ||= RCS::Common::WinFirewall
+  end
+
+  def stub_advfirewall_and_return(filepath)
+    RCS::Common::WinFirewall::Advfirewall.__send__(:define_singleton_method, :call) do |cmd|
+      stubbed_resp = File.read(File.expand_path("../fixtures/advfirewall/#{filepath}", __FILE__))
+      RCS::Common::WinFirewall::AdvfirewallResponse.new(stubbed_resp)
+    end
+  end
+
+  def test_status_on
+    stub_advfirewall_and_return("show_currentprofile_state_on")
+    assert_equal(subject.status, :on)
+  end
+
+  def test_status_off
+    stub_advfirewall_and_return("show_currentprofile_state_off")
+    assert_equal(subject.status, :off)
+  end
+
+  def test_successfully_change_status
+    stub_advfirewall_and_return("command_ok")
+    assert_block { subject.status = :on }
+    assert_block { subject.status = :off }
+  end
+
+  def test_wrong_change_status
+    stub_advfirewall_and_return("command_ok")
+    assert_raise { subject.status = :invalid_value }
+
+    stub_advfirewall_and_return("command_err")
+    assert_raise { subject.status = :on }
+  end
+
+  def test_rules_size
+    stub_advfirewall_and_return("firewall_show_rule_name_all")
+    assert_equal(subject.rules.size, 306)
+  end
+
+  def test_rules_parsing
+    stub_advfirewall_and_return("firewall_show_rule_name_all")
+
+    rule = subject.rules.first
+    expected_attributes = {
+      :enabled=>:yes,
+      :protocol=>:udp,
+      :profiles=>:public,
+      :grouping=>"RCS Firewall Rules",
+      :direction=>:in,
+      :local_ip=>:any,
+      :remote_ip=>:any,
+      :local_port=>:any,
+      :remote_port=>:any,
+      :edge_traversal=>:defer_to_user,
+      :action=>:allow,
+      :rule_name=>"Ruby interpreter (CUI) 2.0.0p247 [i386-mingw32]"
+    }
+    assert_equal(rule.attributes, expected_attributes)
+
+    rule = subject.rules.find { |rule| rule.rule_name == "Media Center Extenders - WMDRM-ND/RTP/RTCP (UDP-In)" }
+    expected_attributes = {
+      :enabled=>:no,
+      :protocol=>:udp,
+      :profiles=>[:domain, :private, :public],
+      :grouping=>"Media Center Extenders",
+      :rule_name=>"Media Center Extenders - WMDRM-ND/RTP/RTCP (UDP-In)",
+      :direction=>:in,
+      :local_ip=>:any,
+      :remote_ip=>"LocalSubnet",
+      :local_port=>[7777,7778,7779,7780,7781,5004,5005,50004,50005,50006,50007,50008,50009,50010,50011,50012,50013],
+      :remote_port=>:any,
+      :edge_traversal=>:no,
+      :action=>:allow
+    }
+    assert_equal(rule.attributes, expected_attributes)
+  end
+end
