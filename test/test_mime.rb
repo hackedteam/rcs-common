c29813e0863209cac18821a23a40b7b4864b6f94
alor <alor@hackingteam.it>
2014-03-07 10:25:02 +0100

    fixed mime tests

--- test/test_mime.rb
+++ test/test_mime.rb
@@ -5,19 +5,19 @@ class TestRcsCommon < Test::Unit::TestCase
   def test_mime_cod
     mime = RCS::MimeType::get('test.cod')
     expected = 'application/vnd.rim.cod'
-    assert_equal mime, expected
+    assert_equal expected, mime
   end
 
   def test_mime_jad
     mime = RCS::MimeType::get('test.jad')
     expected = 'text/vnd.sun.j2me.app-descriptor'
-    assert_equal mime, expected
+    assert_equal expected, mime
   end
 
   def test_mime_jnlp
     mime = RCS::MimeType::get('test.jnlp')
     expected = 'application/x-java-jnlp-file'
-    assert_equal mime, expected
+    assert_equal expected, mime
   end
 
   def test_mime_cab
@@ -25,25 +25,25 @@ class TestRcsCommon < Test::Unit::TestCase
     #expected = 'application/vnd.ms-cab-compressed'
     # fucking IE mobile not understanding the real mime type
     expected = 'binary/octet-stream'
-    assert_equal mime, expected
+    assert_equal expected, mime
   end
 
   def test_mime_apk
     mime = RCS::MimeType::get('test.apk')
     expected = 'application/vnd.android.package-archive'
-    assert_equal mime, expected
+    assert_equal expected, mime
   end
 
   def test_mime_exe
     mime = RCS::MimeType::get('test.exe')
-    expected = 'application/octet-stream'
-    assert_equal mime, expected
+    expected = 'application/x-msdos-program'
+    assert_equal expected, mime
   end
 
   def test_mime_unknown
     mime = RCS::MimeType::get('test.unknown')
     expected = 'binary/octet-stream'
-    assert_equal mime, expected
+    assert_equal expected, mime
   end
 
 end
