7059fb04a189b40eed125493973f7ea6752544cc
alor <alor@hackingteam.it>
2014-04-17 10:01:43 +0200

    better evidence generation

--- lib/rcs-common/evidence/filesystem.rb
+++ lib/rcs-common/evidence/filesystem.rb
@@ -10,23 +10,29 @@ module FilesystemEvidence
 	FILESYSTEM_IS_EMPTY     = 2
 
   def content
-    #path = ["C:\\\\topsecret\\\\bombing plan", "C:\\miao\\bau", "C:\\pippo\\pluto\\paperino", "/home/alor/secret", "/mnt/share/secret"].sample
-    path = ["/home/alor/secret", "/mnt/share/secret"].sample
-    path = path.to_utf16le_binary_null
+    path = ["/home/alor/secret", "/mnt/share/secret", "/mnt/volumes/usb", "/home/test/file", "/file1", "/file2"].sample
+
+    full = path.split(/[\\\/]/)
     content = StringIO.new
-    content.write [FILESYSTEM_VERSION, path.bytesize, FILESYSTEM_IS_FILE, rand(0..2**24), 0].pack("I*")
-    time = Time.now.getutc.to_filetime
-    time.reverse!
-    content.write time.pack('L*')
-    content.write path
+    fp = ''
+
+    begin
+      current = full.shift
+      fp += "/#{current}"
+      fp.gsub!("//", "/")
+      path = fp.to_utf16le_binary_null
+      content.write [FILESYSTEM_VERSION, path.bytesize, (full.size > 0) ? FILESYSTEM_IS_DIRECTORY : FILESYSTEM_IS_FILE, rand(0..2**24), 0].pack("I*")
+      time = Time.now.getutc.to_filetime
+      time.reverse!
+      content.write time.pack('L*')
+      content.write path
+    end until full.size == 0
 
     content.string
   end
   
   def generate_content
-    ret = Array.new
-    10.rand_times { ret << content() }
-    ret
+    [content()]
   end
   
   def decode_content(common_info, chunks)
