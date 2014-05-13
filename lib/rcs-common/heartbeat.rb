261e08dddf2f581e96a8798e041c182551dfd536
topac <topac@hackingteam.com>
2014-05-13 13:48:18 +0200

    refactor heartbeat

--- lib/rcs-common/heartbeat.rb
+++ lib/rcs-common/heartbeat.rb
@@ -6,50 +6,7 @@ module RCS::HeartBeat
   class Base
     extend RCS::Tracer
     include RCS::Tracer
-
-    def initialize(component_name, component_fullname: nil, before_heartbeat: nil, after_heartbeat: nil)
-      @component_name     = component_name.to_s.downcase
-      @component_fullname = component_fullname || "RCS::#{component_name.to_s.capitalize}"
-      @before_heartbeat   = before_heartbeat
-      @after_heartbeat    = after_heartbeat
-    end
-
-    def hostname
-      @hostname ||= Socket.gethostname rescue 'unknown'
-    end
-
-    # @note: This must be implemented by subclasses
-    def message
-      raise("You should implement the message method")
-    end
-
-    def status
-      RCS::SystemStatus.my_status
-    end
-
-    def update_component_status
-      if @before_heartbeat.respond_to?(:call)
-        callback_result = instance_exec(&@before_heartbeat)
-        return if callback_result == false
-      end
-
-      RCS::SystemStatus.reset
-
-      status_str = status.to_s.upcase
-      stats = {:disk => RCS::SystemStatus.disk_free, :cpu => RCS::SystemStatus.cpu_load, :pcpu => RCS::SystemStatus.my_cpu_load(@component_fullname)}
-      msg = RCS::SystemStatus.my_error_msg || message
-
-      if defined?(::Status)
-        # Db
-        ::Status.status_update(@component_fullname, hostname, status_str, msg, stats, @component_name, $version)
-      else
-        # Collector
-        db_class = Object.const_get(@component_fullname)::DB
-        db_class.instance.update_status(@component_fullname, '', status_str, msg, stats, @component_name, $version)
-      end
-    ensure
-       instance_exec(&@after_heartbeat) if @after_heartbeat.respond_to?(:call)
-    end
+    include RCS::SystemStatusCodes
 
     # Declare the component
     # To use as an helper in the subclasses
@@ -58,25 +15,77 @@ module RCS::HeartBeat
       @component_fullname = component_fullname
     end
 
-    def self.before_heartbeat(&block)
-      @before_heartbeat = block
-    end
-
-    def self.after_heartbeat(&block)
-      @after_heartbeat = block
-    end
-
     # This method is called from outside
     def self.perform
-      raise("Undefined component version") unless $version
-      raise("Undefined component name") unless @component_name
-
-      new(@component_name, component_fullname: @component_fullname, before_heartbeat: @before_heartbeat, after_heartbeat: @after_heartbeat).update_component_status
+      heartbeat = new(@component_name, component_fullname: @component_fullname)
+      status, message = *heartbeat.perform if heartbeat.respond_to?(:perform)
+      if status
+        heartbeat.update(status, message)
+      else
+        # trace(:warn, "heartbeat prevented")
+      end
     rescue Interrupt
       trace :fatal, "Heartbeat was interrupted because of a term signal"
     rescue Exception => ex
       trace :fatal, "Cannot perform heartbeat: #{ex.message}"
       trace :fatal, ex.backtrace
+    end
+
+
+    def initialize(component_name, component_fullname: nil)
+      raise("Undefined component version") unless version
+      raise("Undefined component name") unless component_name
+
+      @component_name     = component_name.to_s.downcase
+      @component_fullname = component_fullname || "RCS::#{component_name.to_s.capitalize}"
+    end
+
+    def update(status, message)
+      system_status, system_message = *system_status_and_message
+
+      if status == OK and system_status != OK
+        status, message = system_status, system_message
+      end
+
+      attributes = [@component_fullname, hostname, status, message, machine_stats, @component_name, version]
+
+      if defined?(::Status)
+        # Db
+        ::Status.status_update(*attributes)
+      else
+        # Collector
+        db_class = Object.const_get(@component_fullname)::DB
+        attributes[1] = ''
+        db_class.instance.update_status(*attributes)
+      end
+    ensure
+      reset_system_status_and_message
+    end
+
+    private
+
+    def hostname
+      @hostname ||= Socket.gethostname rescue 'unknown'
+    end
+
+    def reset_system_status_and_message
+      RCS::SystemStatus.reset
+    end
+
+    def system_status_and_message
+      [RCS::SystemStatus.status, RCS::SystemStatus.message]
+    end
+
+    def version
+      $version
+    end
+
+    def machine_stats
+      {
+        disk: RCS::SystemStatus.disk_free,
+        cpu: RCS::SystemStatus.cpu_load,
+        pcpu: RCS::SystemStatus.my_cpu_load(@component_fullname)
+      }
     end
   end
 end
