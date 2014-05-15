244173e56977b05617263500f744e39d55c434dc
topac <topac@hackingteam.com>
2014-05-15 16:32:04 +0200

    more diagnostic info

--- lib/rcs-common/diagnosticable.rb
+++ lib/rcs-common/diagnosticable.rb
@@ -11,15 +11,11 @@ module RCS
       File.expand_path("./log", execution_directory)
     end
 
-    def logs
-      Dir["#{log_path}/*"]
-    end
-
-    def grouped_logs(deepness: 2)
+    def grouped_logs(glob: "#{log_path}/*", deepness: 2)
       groups = Hash.new { |h, k| h[k] = [] }
       regexp = /(rcs\-.+)\_\d{4}-\d{2}-\d{2}\.log|(mongo..log)/
 
-      logs.each do |path|
+      Dir[glob].each do |path|
         filename = File.basename(path)
         group_name = filename.scan(regexp).flatten.compact.first
         groups[group_name] << path if group_name
@@ -33,19 +29,77 @@ module RCS
       groups
     end
 
+    def change_trace_level(level)
+      trace_yaml = "#{execution_directory}/config/trace.yaml"
+      raise "Unable to find file #{trace_yaml}" unless File.exists?(trace_yaml)
+      content = File.read(trace_yaml)
+      content.gsub!(/(name\s*:\s*logfile\n\s*level\s*:\s*)[A-Z]+/, '\1'+level.to_s.upcase)
+      File.open(trace_yaml, "wb") { |f| f.write(content) }
+    end
+
+    def relevant_logs
+      list = grouped_logs.values + grouped_logs(glob: "#{log_path}/err/*", deepness: 7).values
+      list.flatten!
+      list
+    end
+
+    def windows?
+      RbConfig::CONFIG['host_os'] =~ /mingw/
+    end
+
+    def os_lang
+      return unless windows?
+      result = `reg query "hklm\system\controlset001\control\nls\language" /v Installlanguage`
+      result.scan(/REG_SZ\s*(.{4})/).flatten.first
+    end
+
+    def command_output(*args)
+      name = args.shift
+      path = "#{execution_directory}/bin/#{name}"
+      output = `ruby #{path}`
+      "Output of command #{name}\n#{output}\n\n"
+    end
+
+    def config_files
+      paths = []
+      %w[VERSION VERSION_BUILD certs/*.crt certs/*.pem *.yaml *.lic *.crt *.pem].each do |glob|
+        paths += Dir["#{execution_directory}/config/#{glob}"]
+      end
+      paths.flatten!
+      paths.uniq!
+      paths
+    end
+
+    def machine_info
+      hash = {}
+      keys = %w[build_os build_vendor build_cpu build RUBY_VERSION_NAME]
+      hash['rbconfig'] = RbConfig::CONFIG.reject { |key| !keys.include?(key) }
+      hash['os_lang'] = os_lang
+      hash['gem_list'] = `gem list`.split("\n")
+      hash['ENV'] = ENV.reject { |key| !%w[RUBY_VERSION PWD].include?(key) }
+
+      fs = Sys::Filesystem.stat(Dir.pwd)
+
+      hash['filesystem'] = {
+        'path' => fs.path,
+        'size' => (fs.blocks * fs.block_size).to_s_bytes,
+        'free' => (fs.blocks_free * fs.block_size).to_s_bytes,
+      }
+      hash
+    end
+
     def huge_log?(path)
       File.size(path) > 52428800 # 50 megabytes
     end
 
     def hide_addresses(string)
-      mask = "###.###.###.###"
-      string.gsub!(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/, mask)
-      string.gsub!(/([a-zA-Z0-9\-\.]+)\:(4444|443|80|2701\d)/, mask+':\2')
-      string
-    end
+      if $options.respond_to?(:[]) and $options[:hide_addresses]
+        mask = "###.###.###.###"
+        string.gsub!(/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/, mask)
+        string.gsub!(/([a-zA-Z0-9\-\.]+)\:(4444|443|80|2701\d)/, mask+':\2')
+      end
 
-    def relevant_logs
-      grouped_logs.values.flatten
+      string
     end
   end
 end
